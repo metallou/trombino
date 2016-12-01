4ab59f404e197f8ff6d5907d6072ccef147f4ab9
Metallou <kevincastejon13@gmail.com>
2016-12-01 12:56:16 +0100

    upgraded image-handler script, removed test photos

--- .image-handler.sh
+++ .image-handler.sh
@@ -1,3 +1,8 @@
 for file in $(ls .original-photos); do
     convert .original-photos/$file -resize 500x1000 -quality 50 photos/$file
 done
+echo "photos resized and compressed"
+
+convert .original-images/fond.jpg -resize 500x1000 -quality 50 images/fond.jpg
+#convert .original-images/logo.jpg -resize 500x1000 -quality 50 images/logo.jpg
+echo "images resized and compressed"
