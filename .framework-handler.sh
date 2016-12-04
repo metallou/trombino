ec8bc3e0cd3dc68e02de17aeafc749e1858c74e6
Metallou <kevincastejon13@gmail.com>
2016-12-04 13:08:33 +0100

    moved and relinked framework simplonstrap

--- .framework-handler.sh
+++ .framework-handler.sh
@@ -1,2 +1,4 @@
-cp simplonstrap/simplonstrap.css css/
-
+mkdir -p css/
+cp ~/DEV/SIMPLONSTRAP/simplonstrap.css css/
+cp -r ~/DEV/SIMPLONSTRAP/fontello .
+echo "Framework SimplonStrap copied into project"
